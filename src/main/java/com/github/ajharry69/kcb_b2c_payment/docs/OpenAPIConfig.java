eeda7a0792c1a96b09931c840db2e0dfb46b8725
Orinda Harrison (Mitch) <mitchke@duck.com>
2025-04-28 15:01:12 +0300

    Secure application using Keycloak Oauth2

--- src/main/java/com/github/ajharry69/kcb_b2c_payment/docs/OpenAPIConfig.java
+++ src/main/java/com/github/ajharry69/kcb_b2c_payment/docs/OpenAPIConfig.java
@@ -1,8 +1,12 @@
 package com.github.ajharry69.kcb_b2c_payment.docs;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.security.SecuritySchemes;
 import io.swagger.v3.oas.annotations.servers.Server;
 
 
@@ -20,6 +24,17 @@ import io.swagger.v3.oas.annotations.servers.Server;
                 description = "Development",
                 url = "http://localhost:8080"
         )}
+)
+@SecuritySchemes(
+        value = @SecurityScheme(
+                name = "OAuth2",
+                description = "OAuth2 authentication with OpenID Connect",
+                scheme = "Bearer",
+                bearerFormat = "JWT",
+                in = SecuritySchemeIn.HEADER,
+                type = SecuritySchemeType.OPENIDCONNECT,
+                openIdConnectUrl = "${spring.security.oauth2.resourceserver.jwt.issuer-uri}/.well-known/openid-configuration"
+        )
 )
 public class OpenAPIConfig {
 }
--- src/main/java/com/github/ajharry69/kcb_b2c_payment/security/SecurityConfig.java
+++ src/main/java/com/github/ajharry69/kcb_b2c_payment/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.github.ajharry69.kcb_b2c_payment.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -10,25 +11,17 @@ import org.springframework.security.config.annotation.web.configurers.HeadersCon
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
+
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
-    /*@Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
-    private String issuerUri;
-
-    @Bean
-    public JwtDecoder jwtDecoder() {
-        return JwtDecoders.fromIssuerLocation(issuerUri);
-    }*/
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http, JWTAuthConverter converter) throws Exception {
         http
                 .authorizeHttpRequests(authorizeRequests ->
-                                authorizeRequests.anyRequest()
-                                        .permitAll()
-                        /*authorizeRequests
+                        authorizeRequests
                                 .requestMatchers(antMatcher("/h2-console/**")).permitAll()
                                 .requestMatchers(antMatcher("/v3/api-docs/**"),
                                         antMatcher("/swagger-ui/**"),
@@ -36,12 +29,9 @@ public class SecurityConfig {
                                 .requestMatchers(antMatcher(HttpMethod.POST, "/api/v1/payments")).authenticated()
                                 .requestMatchers(antMatcher(HttpMethod.GET, "/api/v1/payments/**")).authenticated()
                                 .anyRequest()
-                                .denyAll()*/
+                                .denyAll()
                 )
-                /*.oauth2ResourceServer(oauth2 -> oauth2.jwt(jwtConfigurer -> {
-                    jwtConfigurer.decoder(jwtDecoder());
-                    jwtConfigurer.jwtAuthenticationConverter(converter);
-                }))*/
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwtConfigurer -> jwtConfigurer.jwtAuthenticationConverter(converter)))
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));
