24f9140fc07454dabccdd27be2f9019412690bdb
Orinda Harrison (Mitch) <mitchke@duck.com>
2025-04-28 15:03:09 +0300

    Enable JPA Auditing

--- src/main/java/com/github/ajharry69/kcb_b2c_payment/KcbB2cPaymentApplication.java
+++ src/main/java/com/github/ajharry69/kcb_b2c_payment/KcbB2cPaymentApplication.java
@@ -2,8 +2,10 @@ package com.github.ajharry69.kcb_b2c_payment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class KcbB2cPaymentApplication {
 
     public static void main(String[] args) {
